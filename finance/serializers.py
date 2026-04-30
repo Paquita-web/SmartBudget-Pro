5d3c41b12c481bd6064ba7fb6c0634c78daac3d1
Alejandra Lucas Simón <Alejandralucasimon@gmail.com>
2026-04-30 20:50:39 +0200

    feat: Load environment variables from .env
    Integrate dotenv to load environment variables, improving local development setup and configuration flexibility.

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Transaction, Budget, Goal, Bill, Profile
+from .models import Transaction, Budget, Goal, Bill, Profile, Asset, ActivityLog
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
